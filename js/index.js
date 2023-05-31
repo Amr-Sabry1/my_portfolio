12e2b16f6644e5a769a999261a3bcbf1d2a332d7
AmrSabryA <95857185+AmrSabryA@users.noreply.github.com>
2023-05-31 18:58:52 +0300

    edit

--- js/index.js
+++ js/index.js
@@ -6,29 +6,40 @@ $(document).ready(function() {
 
 })
 
-let aboutSection = $("#about").offset().top
+// let aboutSection = $("#about").offset().top
 
-$(window).scroll(function() {
-    let windowScroll = $(window).scrollTop();
+// $(window).scroll(function() {
+//     let windowScroll = $(window).scrollTop();
 
-    if (windowScroll > aboutSection + -400) {
-        $('nav .container').css("background-color", "black")
-        $('nav .container').css({ "padding-left": "40px", "padding-right": "40px" })
-        $(".iconarrow i").fadeIn(500)
-    } else {
-        $('nav .container').css("background-color", "rgba(0,0,0,0)")
-        $('nav .container').css({ "padding-left": "0px", "padding-right": "0px" })
+//     if (windowScroll > aboutSection + -400) {
+//         $('nav .container').css("background-color", "black")
+//         $('nav .container').css({ "padding-left": "40px", "padding-right": "40px" })
+//         $(".iconarrow i").fadeIn(500)
+//     } else {
+//         $('nav .container').css("background-color", "rgba(0,0,0,0)")
+//         $('nav .container').css({ "padding-left": "0px", "padding-right": "0px" })
 
-        $(".iconarrow i").fadeOut(500)
+//         $(".iconarrow i").fadeOut(500)
 
-    }
-})
+//     }
+// })
 
 
-$(".nav-item a").click(function(e) {
-    let navClick = $(e.target).attr("href")
-    $(e.target).css({ color: "yellow" });
-    $(e.target).parent().siblings().children().css({ color: "white" })
-    let sectionOffset = $(navClick).offset().top;
-    $("html,body").animate({ scrollTop: sectionOffset })
-})
+// $(".nav-item a").click(function(e) {
+//     let navClick = $(e.target).attr("href")
+//     $(e.target).css({ color: "yellow" });
+//     $(e.target).parent().siblings().children().css({ color: "white" })
+//     let sectionOffset = $(navClick).offset().top;
+//     $("html,body").animate({ scrollTop: sectionOffset })
+// })
+
+
+const list = document.querySelectorAll('.list');
+
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
